d81c1ea52df3ab0a6e297929f7a00641fec60b41
Awat Singdamrong <a.lubing@yahoo.com>
2024-01-29 23:17:56 +0700

    Add simple sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -8,20 +8,29 @@ function App() {
 
   return (
     <div className='flex flex-col'>
-      <main className='w-full min-h-screen bg-slate-100'>
+      <header>
         <h1 className="text-3xl font-bold underline bg-red-400">
           Hello world!
         </h1>
-        <div className='m-10'>
-          <Item />
-        </div>
-        <div className='m-10'>
-          <AddItemModal />
-        </div>
+      </header>
+      <div className='flex flex-row'>
+        <aside className='w-[20%] bg-white flex flex-col'>
+          <div className='mx-5 mt-10'>
+            <AddItemModal />
+          </div>
+        </aside>
+        <main className='w-full min-h-screen bg-slate-100'>
+
+          <div className='m-10'>
+            <Item />
+          </div>
 
 
 
-      </main>
+
+        </main>
+      </div>
+
 
     </div>
 
