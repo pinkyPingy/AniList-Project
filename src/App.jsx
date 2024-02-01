7a79e4dce57cf4b60e202fe7180ff4ad7aa627d9
Awat Singdamrong <a.lubing@yahoo.com>
2024-02-02 03:05:07 +0700

    add interactive sidebar in sm

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,11 @@ import axios from 'axios';
 
 function App() {
   const [items, setItems] = useState([]);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  }
 
   useEffect(() => {
     // Fetch data from backend when component mounts
@@ -22,14 +27,26 @@ function App() {
 
   return (
     <div className='flex flex-col'>
-      <header className='bg-[#4F5D75]'>
-        <h1 className="text-3xl text-white inline-block ml-5 my-3">
+      <header className='bg-[#4F5D75] z-30 w-full'>
+        <button
+          className="md:hidden fixed top-0 left-0 my-2 mx-3 text-4xl z-40 text-[#EF8354]"
+          onClick={toggleSidebar}
+        >
+          ☰
+        </button>
+        <h1 className="text-3xl text-white inline-block ml-16 md:ml-5 my-3">
           AniList-Project
         </h1>
       </header>
+      {isSidebarOpen && (
+        <div
+          className="fixed top-0 left-0 w-full h-full bg-black opacity-50 z-10"
+          onClick={toggleSidebar}
+        />
+      )}
       <div className='flex flex-row'>
 
-        <aside className='w-[20%] bg-white flex flex-col'>
+        <aside className={`w-[40%] md:w-[20%] z-20 h-full ${isSidebarOpen ? 'fixed' : 'hidden'} md:block bg-white flex flex-col`}>
           <div className='mx-5 mt-10'>
             <AddItemModal setItems={setItems} />
           </div>
--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -86,15 +86,15 @@ export default function Item({ item, setItems }) {
     }
 
     return (
-        <div className="md:w-[760px] md:h-[142px] bg-white flex flex-col md:flex-row rounded-lg py-[10px] px-[10px] mb-3 shadow-md">
+        <div className="lg:w-[760px] md:h-[142px] bg-white flex flex-col md:flex-row rounded-lg py-[10px] px-[10px] mb-3 shadow-md">
             {/* image */}
             <div className="flex flex-row mb-3 md:mb-0">
-                <img src={item.imgURL} className="rounded-md mr-[10px] md:mr-[22px] w-[86.85px]" />
+                <img src={item.imgURL} className="rounded-md mr-[13px] md:mr-[22px] w-[86.85px]" />
 
                 <div className="flex flex-col justify-between">
                     <div>
                         <div><span className="text-[#2D3142] font-semibold text-xl md:text-2xl">{item.name}</span></div>
-                        <div><span className="text-[#BFC0C0] text-xs">Genres: {item.genres}</span></div>
+                        <div><span className="text-[#BFC0C0] text-xs md:text-sm">Genres: {item.genres}</span></div>
                     </div>
                     <div><span className="text-[#4F5D75]">{item.note}</span></div>
                 </div>
